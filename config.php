97ae71c5bd96a8ee9d58124f2401e783f565acd0
gabrielcedro <43965031+gabrielcedro@users.noreply.github.com>
2019-08-19 15:38:47 -0300

    Add files via upload
    MVC Simples

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+    $config = [
+        
+        'db' => [
+            'host' => 'localhost',
+            'user' => 'root',
+            'pass' => 'root',
+            'dbname' => 'mvc'
+        ]
+
+    ];
+
+    // TEMA USADO: Link do tema utilizado
+    
+    define('NETSIMPLES', 'Desenvolvido e administrado por <a href="http://netsimples.com.br"><img src="assets/images/logos/netsimples.png" width="100px" alt="netsimples"></a>');
+
+    define('TEL','(27) 3333-3333');
+
+    define('TEL2','(27) 99999-9999');
+
+    define('END','Endreço 1');
+
+    define('END2','Endereço 2');
+
+    define('MAIL','Email 1');
+
+    define('MAIL2','Email 2');
+
+    define('TITULO','Titulo');
+
+    define('FACEBOOK', 'Link Facebook');
+
+    define('INSTAGRAM', 'Link Instagram');
